0dd8ae906d56c54140d977bf214e211657b774f8
cuiliang0302 <42726109+cuiliang0302@users.noreply.github.com>
2020-08-24 09:30:11 +0800

    修复退出登录页手机版样式

--- account/forms.py
+++ account/forms.py
@@ -5,6 +5,13 @@ class LoginForm(forms.Form):
     # 输入用户名密码登录表单
     username = forms.CharField(max_length=128,
                                error_messages={'required': '用户名不能为空'},
-                               widget=forms.TextInput(attrs={'class': 'form-style'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-style'}))
-
+                               widget=forms.TextInput(attrs={'class': 'form-style',
+                                                             'placeholder': '用户名',
+                                                             'oninvalid': 'setCustomValidity("请输入用户名");',
+                                                             'oninput': 'setCustomValidity("");'}))
+    password = forms.CharField(max_length=256,
+                               error_messages={'required': '密码不能为空'},
+                               widget=forms.PasswordInput(attrs={'class': 'form-style',
+                                                                 'placeholder': '密码',
+                                                                 'oninvalid': 'setCustomValidity("请输入密码");',
+                                                                 'oninput': 'setCustomValidity("");'}))
