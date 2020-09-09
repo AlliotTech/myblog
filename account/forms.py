04a029ad25fec073d1f8deaa622356d2a9130bda
cuiliang0302 <42726109+cuiliang0302@users.noreply.github.com>
2020-09-09 18:15:47 +0800

    修改重置密码js注释

--- account/forms.py
+++ account/forms.py
@@ -8,7 +8,7 @@ from account.models import UserInfo
 class LoginForm(forms.Form):
     # 输入用户名密码登录表单
     user = forms.CharField(
-        max_length=16,
+        max_length=10,
         error_messages={'required': '用户名不能为空'},
         widget=forms.TextInput(attrs={'class': 'form-style',
                                       'placeholder': '用户名',
